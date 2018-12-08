34fba7e50efa8b800e7ac7895118dbd92c6c2aec
marcinrusiecki <marcinrusiecki03@gmail.com>
2018-12-08 11:11:46 +0100

    init

---
+++ zadanie1.py
@@ -0,0 +1,25 @@
+#Policz pole trójkąta o podstawie 10 i wysokości 5
+a = 10
+b = 5
+c = (a/2)*b
+print ("Pole trójkąta wynosi ",c)
+
+#Policz pole koła o promieniu 7
+
+r = 7
+p = 3.14
+c = p*(r**2)
+print("Pole koła wynosi ",c) 
+#Policz pole trapezu o długości podstaw  i  i wysokości 5
+
+t1 = 3
+t2 = 9
+h = 5
+w = 1/2 * (3+9)*h
+print("Pole koła wynosi ",w) 
+#Policz objętość kuli o zadanym promieniu.
+r= 10
+q= 4/3*3.14*r**
+print("Pole koła wynosi ",q) 
+
+
